d6a433d71bd416df1878df7e4e5d886975d17b77
nikaqilbot <66864213+nikaqilbot@users.noreply.github.com>
2021-01-07 23:29:48 +0800

    gui-nikaqilbot

--- Action.py
+++ Action.py
@@ -1,16 +1,15 @@
 import tello
 import Perimeter_Sweep
 
-class Action:  
 
-  def connectToDrone(self):
-    print('Initilize connection to tello drone')
-    self.drone = tello.Tello()
-    print('Success connect to tello drone')
+def connectToDrone():
+  print('Initilize connection to tello drone')
+  self.drone = tello.Tello()
+  print('Success connect to tello drone')
 
-  def startPerPlanRoute(self):
-    print('Start Pre Plan Route')
-    Perimeter_Sweep.PrePlanRoute(self.drone).start()
-    print('End Pre Plan Route')
+def startPerPlanRoute():
+  print('Start Pre Plan Route')
+  Perimeter_Sweep.PrePlanRoute(self.drone).start()
+  print('End Pre Plan Route')
 
 
